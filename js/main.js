59489a63e74fb23fe563468ad7b333e1caf1905b
Sam Morris <mail@sammorrisdesign.co.uk>
2014-10-11 00:15:18 +0100

    Make blocks clickable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-// Place your javascripts here
+$(".block").click(function() {
+    $(this).attr("class", "block block--color-" + (Math.floor(Math.random() * 30) + 1) + " block--position-" + (Math.floor(Math.random() * 30) + 1) + " block--size-" + (Math.floor(Math.random() * 30) + 1));
+});
