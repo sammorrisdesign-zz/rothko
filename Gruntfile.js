829e56bb69a13c319ae02966e6958403918d62bc
Sam Morris <mail@sammorrisdesign.co.uk>
2014-10-10 23:24:03 +0100

    Initial commit from Mies

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    watch: {
+      scsslint: {
+        files: 'scss/**/*.scss',
+        tasks: ['scsslint']
+      },
+      css: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+            'style.css' : 'scss/style.scss'
+        }
+      }
+    },
+    scsslint: {
+      allFiles: [
+        'scss/**/*.scss'
+      ],
+      options: {
+        config: 'scss/.scss-lint.yml'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-scss-lint');
+
+  // Default task.
+  grunt.registerTask('default', ['sass', 'scsslint', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+Mies Boilerplate
+================
+
+Mies Boilerplate is a project boilerplate for the web. It was designed so I could spend less time setting up projects and more time actually working on them, maybe you'll get some use out of it too.
+
+## Usage
+
+Initial install to get dependencies; `bundle install` and `npm install`. Then use `Grunt` to watch and lint the sass.
+
+## Background
+
+The project is named after the architect, [Ludwig Mies van der Rohe](http://en.wikipedia.org/wiki/Ludwig_Mies_van_der_Rohe), who was one of the pioneers of modernism. It seemed appropriate for a project boilerplate when GitHub wanted a name from me.
