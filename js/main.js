ab001bef693cfd26d3591bc5e83cb8797725d0ac
Sam Morris <mail@sammorrisdesign.co.uk>
2014-10-13 22:05:48 +0100

    Replace position with spacing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function changeBlock(el) {
+    $(el).attr("class", "block block--color-" + (Math.floor(Math.random() * 30) + 1) + " block--size-" + (Math.floor(Math.random() * 30) + 1) + " block--spacing-" + (Math.floor(Math.random() * 30) + 1));
+}
+
 $(".block").click(function() {
-    $(this).attr("class", "block block--color-" + (Math.floor(Math.random() * 30) + 1) + " block--position-" + (Math.floor(Math.random() * 30) + 1) + " block--size-" + (Math.floor(Math.random() * 30) + 1));
+    changeBlock(this);
 });
