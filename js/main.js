aa883c9dae63955e049b298fd5a1ca73954f4f47
Sam Morris <mail@sammorrisdesign.co.uk>
2014-10-13 22:17:01 +0100

    Randomise background colour

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ function changeBlock(el) {
 $(".block").click(function() {
     changeBlock(this);
 });
+
+window.setInterval(function() {
+    var block = $(".block:nth-of-type(" + (Math.floor(Math.random() * 4) + 1) + ")");
+    changeBlock(block);
+}, 5000);
+
+window.setInterval(function() {
+    $("body").attr("class", "body--color-" + (Math.floor(Math.random() * 30) + 1));
+}, 5000);
